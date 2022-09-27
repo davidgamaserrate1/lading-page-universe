2cb4f3b6f4684eccb8aefcd13aaaedf9815a0947
davidgamaserrate1 <Ur GitHub email>
2022-09-26 22:39:33 -0400

    feat: Venue Info Card : Location Card : Date

--- src/components/header_footer/Footer.js
+++ src/components/header_footer/Footer.js
@@ -5,7 +5,7 @@ const Footer = () => {
         <footer className="bck_red">
             <Fade triggerOnce delay={500}>
                 <div className="font_righteous footer_logo_venue">Landing Page</div>
-                <div className="footer_copyright">Developed with Love by <a className="profileDev" href="https://www.instagram.com/_david.gama/" >@David</a>🧡</div>
+                <div className="footer_copyright">Developed with Love by <a className="redirectLink" href="https://www.instagram.com/_david.gama/" >@David</a>🧡</div>
             </Fade>
 
         </footer>
--- src/components/venueInfo/index.js
+++ src/components/venueInfo/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import { Zoom } from "react-awesome-reveal";
 
-import icon_calendar from '../../resources/images/icons/calendar.png'
+//import icon_calendar from '@mui/icons-material/CalendarMonth' ;
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonthIcon';
+//import icon_calendar from '../../resources/images/icons/calendar.png'
 import icon_location from '../../resources/images/icons/location.png'
+import BackgroundImg from '../../resources/images/background.jpg'
+
 const VenueNfo = () =>{
-    return(
-        <div className="bck_black">
+    return( 
+        <div className="bck_black"  style={{ backgroundImage:`url(${BackgroundImg})`  }}>
             <div className="center_wrapper">
                 <div className="vn_wrapper">
 
@@ -13,13 +17,8 @@ const VenueNfo = () =>{
                         <div>
                             <div className="vn_outer">
                                 <div className="vn_inner">
-                                    <div className="vn_icon_square bck_red"></div>
-                                    <div 
-                                        className="vn_icon"
-                                        style={{
-                                            backgroudColor:`url(${icon_calendar})`
-                                        }}
-                                    ></div>
+                                    <div className="vn_icon_square bck_red">  <CalendarMonthIcon/> </div>
+                                   
                                     <div className="vn_title">
                                         Event & Date 
                                     </div>
@@ -35,17 +34,15 @@ const VenueNfo = () =>{
                             <div className="vn_outer">
                                 <div className="vn_inner">
                                     <div className="vn_icon_square bck_yellow"></div>
-                                    <div 
-                                        className="vn_icon"
-                                        style={{
-                                            backgroudColor:`url(${icon_location})`
-                                        }}
-                                    ></div>
+                                    <div className="vn_icon" > 
+                                                                            
+                                    </div>
+                                    
                                     <div className="vn_title">
                                         Localização
                                     </div>
                                     <div className="vn_desc">
-                                        FACOM - UFMS
+                                        <a className="redirectLink" href="https://www.google.com/maps?q=facom+ufms"> FACOM - UFMS </a> 
                                     </div>
                                 </div> 
                             </div>  

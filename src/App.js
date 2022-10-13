6ed1b8120162c013cfac39fcb208d9de568760ef
davidgamaserrate1 <Ur GitHub email>
2022-10-13 10:11:43 -0400

    feat : Highlights conclued

--- src/App.js
+++ src/App.js
@@ -8,7 +8,6 @@ import BackgroundImg from './resources/images/background.jpg'
 import VenueNfo from "./components/venueInfo"; 
 import Highlights from "./components/highlights";
 
-
 //const urlImg = './resources/images/' ;
 
 const App = () => {
@@ -26,10 +25,12 @@ const App = () => {
       <>
         <Highlights/>
       </>   
+      
 
         <div style={{ backgroundImage:`url(${BackgroundImg})`,  backgroundColor: '#3c3c3c', height: '1500px' }}> </div>
         <Footer />
       </>
+
       
     </div>
  
--- src/components/highlights/Probability.js
+++ src/components/highlights/Probability.js
@@ -1,23 +1,51 @@
 import React,{useState,useEffect} from "react";
 import { Fade,Slide } from "react-awesome-reveal";
+
 const Probability = () => {
+    
+    const end = 30;
+    const [start, setStart] = useState(0);
+
+    const porcentage = ()  =>{
+        if(start < end)
+            setStart(prevCount => prevCount + 1)
+        
+    }
+
+    useEffect(()=>{
+        if(start > 0 && start < end){
+            setTimeout(()=>{
+            setStart(prevCount => prevCount + 1)
+            },end)
+        }
+    },[start] )
+
     return (
         <div className="center_wrapper">
             <div className="discount_wrapper">
 
-                <Fade>
+                <Fade 
+                    onVisibilityChange={(inView) =>{
+                        if(inView){
+                            porcentage()
+                        }
+                    }}>
+
+                
+
+               
                       <div className="discount_porcentage">                      
-                        <span>0%</span>
-                        <span>Visibility in Campo Grande</span>
+                        <span>{start}%</span>
+                        <span>Visibilidade em Campo Grande</span>
                     </div>
 
                 </Fade>
 
                 <Slide righ={true}>
                     <div className="discount_description">
-                        <h3>20 de Abril de 2023 Total Solar Eclipse</h3>
+                        <h3>20 de Abril de 2023 Total </h3>
                         <p>
-
+                        Solar Eclipse
                         </p>
                     </div>
                 </Slide>
